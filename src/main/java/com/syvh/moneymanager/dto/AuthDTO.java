d60794feea8fa8b6c9f8db987bc1261441e17ca9
vuhuusy <vuhuusytft1@gmail.com>
2026-03-14 21:18:56 +0700

    feat(dto): add auth and profile data transfer objects

---
+++ src/main/java/com/syvh/moneymanager/dto/AuthDTO.java
@@ -0,0 +1,17 @@
+package com.syvh.moneymanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthDTO {
+
+    private String email;
+    private String password;
+    private String activationToken;
+}
---
+++ src/main/java/com/syvh/moneymanager/dto/ProfileDTO.java
@@ -0,0 +1,23 @@
+package com.syvh.moneymanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProfileDTO {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String profileImageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
